8497325bf9bda366f40eea7faffa7594c4130a46
kawtar Smougueni <ksmougueni@sqli.com>
2022-04-10 10:52:53 +0000

    k

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
 import logo from './logo.svg';
 import './App.css';
+import React,{useState} from 'react';
+import MaterialTable from 'material-table';
+import { Button } from '@material-ui/core'
+import AddBox from "@material-ui/icons/AddBox";
+import ArrowDownward from "@material-ui/icons/ArrowDownward";
 
 function App() {
+  const [tableData,setTableData]=useState([])
+  const columns=[
+   {title:"Name",field:"name"},
+   {title:"Email",field:"email"},
+   {title:"Phone Number",field:"phone"},
+   {title:"Age",field:"age"},
+   {title:"City",field:"city"},
+  ]
+
+  /*const data = [
+    ["Joe James", "j@gmail.com", "06366987", "12","rabat"],
+     ["kawtar", "k@gmail.com", "0637363026", "22","sale"]
+    
+  ];*/
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <h1 align="center">React app </h1>
+    <h4 align="center">Crash course onn meterial trable</h4>
+    <MaterialTable columns={columns} data={tableData} />
     </div>
   );
 }
